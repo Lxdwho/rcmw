9f56b26a6a18dd39ea815b52adfe797735f44980
lxd <2205312781@qq.com>
2025-10-29 20:52:52 +0800

    1029: 完成了原子哈希表并进行了简单测试

---
+++ base/atomic_hash_map.h
@@ -0,0 +1,288 @@
+/**
+ * @brief   cmw 原子哈希表仿写
+ * @date    2025.10.29
+ * @author  lxd
+ */
+
+#ifndef RCMW_BASE_ATOMIC_HASH_MAP_H_
+#define RCMW_BASE_ATOMIC_HASH_MAP_H_
+
+#include <atomic>
+#include <utility>
+
+namespace hnu  {
+namespace rcmw {
+namespace base {
+
+/**
+ * @brief   原子操作实现的无锁固定大小哈希表
+ * @tparam
+ */
+template <typename K, typename V, std::size_t TableSize = 128, 
+          typename std::enable_if<std::is_integral<K>::value &&
+          (TableSize & (TableSize - 1)) == 0, int>::type = 0>
+class AtomicHashMap {
+public:
+    AtomicHashMap() : capacity_(TableSize), mode_num_(capacity_ - 1) {}
+    AtomicHashMap(const AtomicHashMap &orther) = delete;
+    AtomicHashMap &operator=(const AtomicHashMap &other) = delete;
+
+    bool Has(K key) {
+        uint64_t index = key & mode_num_;
+        return table_[index].Has(key);
+    }
+
+    bool Get(K key, V **value) {
+        uint64_t index = key & mode_num_;
+        return table_[index].Get(key, value);
+    }
+
+    bool Get(K key, V *value) {
+        uint64_t index = key & mode_num_;
+        V *val = nullptr;
+        bool res = table_[index].Get(key, &val);
+        if(res) {
+            *value = *val;
+        }
+        return res;
+    }
+
+    void Set(K key) {
+        uint64_t index = key & mode_num_;
+        table_[index].Insert(key);
+    }
+
+    void Set(K key, const V &value) {
+        uint64_t index = key & mode_num_;
+        table_[index].Insert(key, value);
+    }
+
+    void Set(K key, V &&value) {
+        uint64_t index = key & mode_num_;
+        table_[index].Insert(key, std::forward<V>(value));
+    }
+
+private:
+    /* Entry 链表节点*/
+    struct  Entry
+    {
+        /* init and end */
+        Entry() {}
+        explicit Entry(K key) : key(key) {
+            value_ptr.store(new V(), std::memory_order_release);
+        }
+        Entry(K key, const V &value) : key(key) {
+            value_ptr.store(new V(value), std::memory_order_release);
+        }
+        Entry(K key, V &&value) : key(key) {
+            value_ptr.store(new V(std::forward<V>(value)), std::memory_order_release);
+        }
+        ~Entry() {delete value_ptr.load(std::memory_order_acquire);}
+        /* param */
+        K key = 0;
+        std::atomic<V *> value_ptr = {nullptr};
+        std::atomic<Entry *> next = {nullptr};
+    };
+
+    /* Bucket 链表 */
+    class Bucket {
+    public:
+        Bucket() : head_(new Entry()) {}
+        ~Bucket() {
+            Entry *ite = head_;
+            while (ite) {
+                auto tmp = ite->next.load(std::memory_order_acquire);
+                delete ite;
+                ite = tmp;
+            }
+        }
+        
+        /* 查找 Bucket 中是否存在 key */
+        bool Has(K key) {
+            Entry *m_target = head_->next.load(std::memory_order_acquire);
+            while(Entry *target = m_target) {
+                if(target->key < key) {
+                    m_target = target->next.load(std::memory_order_acquire);
+                    continue;
+                }
+                else {
+                    return target->key == key;
+                }
+            }
+            return false;
+        }
+
+        /* 查找 Bucket 中的 key */
+        bool Find(K key, Entry **prev_ptr, Entry **target_ptr) {
+            Entry *prev = head_;
+            Entry *m_target = head_->next.load(std::memory_order_acquire);
+            while(Entry *target = m_target) {
+                if(target->key == key) {
+                    *prev_ptr = prev;
+                    *target_ptr = target;
+                    return true;
+                }
+                else if(target->key > key) {
+                    *prev_ptr = prev;
+                    *target_ptr = target;
+                    return false;
+                }
+                else {
+                    prev = target;
+                    m_target = target->next.load(std::memory_order_acquire);
+                }
+            }
+            *prev_ptr = prev;
+            *target_ptr = nullptr;
+            return false;
+        }
+
+        /* 拷贝构造插入 */
+        void Insert(K key, const V &value) {
+            Entry *prev = nullptr;
+            Entry *target = nullptr;
+            Entry *new_entry = nullptr;
+            V *new_value = nullptr;
+            while(true) {
+                if(Find(key, &prev, &target)) {
+                    if(!new_value) {
+                        new_value = new V(value);
+                    }
+                    // 拿到当前 key 的值 old_val_ptr
+                    auto old_val_ptr = target->value_ptr.load(std::memory_order_acquire);
+                    // 线程安全操作：如果别的线程先一步对当前 key 进行了改动，
+                    // 则返回false，把 value_ptr 当前值写回 old_val_ptr，等待下次修改
+                    // 如果成功写入，则返回true，将 new_value 写入 target->value_ptr
+                    if(target->value_ptr.compare_exchange_strong(old_val_ptr, new_value, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        delete old_val_ptr;
+                        if(new_entry) {
+                            delete new_entry;
+                            new_entry = nullptr;
+                        }
+                        return;
+                    }
+                    continue;
+                }
+                else {
+                    if(!new_entry) {
+                        new_entry = new Entry(key, value);
+                    }
+                    new_entry->next.store(target, std::memory_order_release);
+                    if(prev->next.compare_exchange_strong(target, new_entry, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        if(new_value) {
+                            delete new_value;
+                            new_value = nullptr;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+        /* 移动构造插入 */
+        void Insert(K key, V &&value) {
+            Entry *prev = nullptr;
+            Entry *target = nullptr;
+            Entry *new_entry = nullptr;
+            V *new_value = nullptr;
+            while(true) {
+                if(Find(key, &prev, &target)) {
+                    if(!new_value) {
+                        new_value = new V(std::forward<V>(value));
+                    }
+                    auto old_val_ptr = target->value_ptr.load(std::memory_order_acquire);
+                    if(target->value_ptr.compare_exchange_strong(old_val_ptr, new_value, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        delete old_val_ptr;
+                        if(new_entry) {
+                            delete new_entry;
+                            new_entry = nullptr;
+                        }
+                        return;
+                    }
+                    continue;
+                }
+                else {
+                    if(!new_entry) {
+                        new_entry = new Entry(key, value);
+                    }
+                    new_entry->next.store(target, std::memory_order_release);
+                    if(prev->next.compare_exchange_strong(target, new_entry, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        if(new_value) {
+                            delete new_value;
+                            new_value = nullptr;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+        /* 拷贝构造2 */
+        void Insert(K key) {
+            Entry *prev = nullptr;
+            Entry *target = nullptr;
+            Entry *new_entry = nullptr;
+            V *new_value = nullptr;
+            while (true)
+            {
+                if(Find(key, &prev, &target)) {
+                    if(!new_value) {
+                        new_value = new V();
+                    }
+                    auto old_val_ptr = target->value_ptr.load(std::memory_order_acquire);
+                    if(target->value_ptr.compare_exchange_strong(old_val_ptr, new_value, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        delete old_val_ptr;
+                        if(new_entry) {
+                            delete new_entry;
+                            new_entry = nullptr;
+                        }
+                        return;
+                    }
+                    continue;
+                }
+                else {
+                    if(!new_entry) {
+                        new_entry = new Entry(key);
+                    }
+                    new_entry->next.store(target, std::memory_order_release);
+                    if(prev->next.compare_exchange_strong(target, new_entry, 
+                        std::memory_order_acq_rel, std::memory_order_relaxed)) {
+                        if(new_value) {
+                            delete new_value;
+                            new_value = nullptr;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+
+        bool Get(K key, V **value) {
+            Entry *prev = nullptr;
+            Entry *target = nullptr;
+            if(Find(key, &prev, &target)) {
+                *value = target->value_ptr.load(std::memory_order_acquire);
+                return true;
+            }
+            return false;
+        }
+
+    private:
+        Entry *head_;
+    };
+
+    Bucket table_[TableSize];   // 哈希链表组
+    uint64_t capacity_;         // 最大存储数
+    uint64_t mode_num_;         // 
+};
+
+} // namespace hnu 
+} // namespace rcmw
+} // namespace base
+
+#endif
