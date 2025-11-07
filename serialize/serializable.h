bbf4372f9b6e8eea1e7d2451a3ccef7e47fbeca7
lxd <2205312781@qq.com>
2025-11-07 21:24:15 +0800

    1107: 完成了序列化部分，待测试、说明文档。

---
+++ serialize/serializable.h
@@ -0,0 +1,43 @@
+/**
+* @brief 数据序列化接口
+* @date  2025.11.07
+*/
+
+#ifndef _SERIALIZEABLE_H_
+#define _SERIALIZEABLE_H_
+
+#include "data_stream.h"
+
+namespace hnu		{
+namespace rcmw		{
+namespace serialize	{
+
+class DataStream;
+
+class Serializable {
+public:
+    virtual void serialize(DataStream& stream) const = 0;
+    virtual bool unserialize(DataStream& stream) = 0;
+};
+
+#define SERIALIZE(...)                              \
+                                                    \
+    void serialize(DataStream& stream) const {      \
+        char type = DataStream::CUSTOM;             \
+        stream.write((char*)&type, sizeof(char));   \
+        stream.write_args(__VA_ARGS__);             \
+    }                                               \
+                                                    \
+    bool unserialize(DataStream& stream) {          \
+        char type;                                  \
+        stream.read(&type, sizeof(char));           \
+        if(type != DataStream::CUSTOM) return false;\
+        stream.read_args(__VA_ARGS__);              \
+        return true;                                \
+    }                                               
+
+}
+}
+}
+
+#endif
